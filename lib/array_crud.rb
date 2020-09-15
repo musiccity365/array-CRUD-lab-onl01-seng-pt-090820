54e10dc8b51a05988d89e5ce33a37249a15dd0b9
Michael David Sackstein <m_sackstein@yahoo.com>
2020-09-15 20:22:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["up", "down", "left", "right"] 
+  array = ["north", "south", "east", "west"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  array << "northeast"
 end
 
 def add_element_to_start_of_array(array, element)
